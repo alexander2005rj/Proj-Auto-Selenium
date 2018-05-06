b523f7af89b4aed1bded94bf85a91641cc321bf6
Alexander Ribeiro <alexander2005rj@gmail.com>
2018-05-06 19:08:36 -0300

    Criando DriverFactory

--- src/main/java/br/com/github/alexander2005rj/core/DriverFactory.java
+++ src/main/java/br/com/github/alexander2005rj/core/DriverFactory.java
@@ -1,5 +1,27 @@
 package br.com.github.alexander2005rj.core;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverFactory {
+	private WebDriver driver;
+	
+	private DriverFactory() {}
+	
+	public WebDriver getDriver() {
+		if ( driver == null ) {
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		return driver;
+	}
+	
+	public void destroyDriver() {
+		if ( driver != null  ) {
+			driver.quit();
+			driver = null;
+		}
+	}
+	
 	
 }
